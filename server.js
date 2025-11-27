e340c7af36d8ed492a061b1fa7e8f7baf4b2250e
Bhuvaneswari-tech <bhuvanagitrepo@gmail.com>
2025-11-27 19:16:46 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const helloRouter = require('./routes/hello');
+
+app.use(express.json());
+
+// Routes
+app.use('/hello', helloRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Simple Node.js App on Azure!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
