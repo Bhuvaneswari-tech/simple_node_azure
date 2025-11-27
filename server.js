a8d9879ccf4528c6ec12e85902b53900b0a932e5
Bhuvaneswari-tech <bhuvanagitrepo@gmail.com>
2025-11-27 19:20:06 +0530

    Initial commit

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
   res.send('Welcome to Simple Node.js App on Azure!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
